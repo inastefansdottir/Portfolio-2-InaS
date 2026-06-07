85da76b636f4edb302b79d2f199df84e56be813f
Ína S Stefánsdóttir <znaste03416@stud.noroff.no>
2026-06-07 13:52:44 +0200

    Made the project hero responsive.

--- src/components/ProjectHero.jsx
+++ src/components/ProjectHero.jsx
@@ -17,18 +17,18 @@ export default function ProjectHero({
   return (
     <section
       className={clsx(
-        "flex h-[650px] justify-center bg-linear-to-t from-background px-4 sm:px-10",
+        "flex h-[650px] max-[1100px]:h-[550px] max-[900px]:h-full max-[900px]:pt-25 justify-center bg-linear-to-t from-background px-4 sm:px-10",
         className
       )}
       style={{ "--tw-gradient-to": gradientTo }}
     >
-      <div className="flex w-full max-w-[1300px] items-center justify-between gap-12">
-        <div className="flex w-[570px] flex-col gap-2">
+      <div className="flex max-[900px]:flex-col w-full max-w-[1300px] max-[1100px]:text-sm items-center justify-between gap-12">
+        <div className="flex max-w-[570px] w-full flex-col gap-2">
           {logo && !hideLogo && (
             <img src={logo} alt="" className="h-6 self-start" />
           )}
 
-          <h1 className="text-[40px]">{title}</h1>
+          <h1 className="max-[1100px]:text-3xl text-[40px]">{title}</h1>
 
           {meta && <p>{meta}</p>}
 
@@ -41,7 +41,7 @@ export default function ProjectHero({
           )}
 
           {links.length > 0 && (
-            <div className="mt-6 flex gap-5">
+            <div className="w-[350px] mt-6 flex gap-5">
               {links.map((link) => (
                 <Button
                   key={link.label}
@@ -56,7 +56,7 @@ export default function ProjectHero({
           )}
         </div>
 
-        <div className="max-w-[620px] w-full">
+        <div className="max-[1100px]:max-w-[500px] max-w-[620px] w-full">
           {image && (
             <img src={image} alt={imageAlt} className="object-cover w-full" />
           )}
